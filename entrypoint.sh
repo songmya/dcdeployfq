6bfe4c485e6d36448345f90187ae28a0d31f7b9f
songmya <89603466+songmya@users.noreply.github.com>
2026-05-25 23:35:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
+WSM=${WSM:-'/m'}
+WSL=${WSL:-'/l'}
+sed -i "s#UUID#$UUID#g;s#WSM#${WSM}#g;s#WSL#${WSL}#g" config.json
+sed -i "s#WSM#${WSM}#g;s#WSL#${WSL}#g" /etc/nginx/nginx.conf
 
 # 伪装 v2ray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
